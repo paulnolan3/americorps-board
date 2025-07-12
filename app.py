9202ca1ace53c9a35d20923d55f999a49470daa0
paulnolan3 <144174178+paulnolan3@users.noreply.github.com>
2025-07-12 18:44:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,8 @@ st.markdown("""
     background-color: transparent;
     color: #1550ed;
     padding: 8px 16px;
-    border-radius: 9999px;     /* ← make this huge for a perfect pill */
+    border-radius: 9999px;
+    margin-bottom: 16px;
     font-size: 1em;
     transition: background-color 0.2s, color 0.2s;
     text-decoration: none !important;
